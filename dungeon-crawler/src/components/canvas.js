b25dd46aeb9c7bb7b33915e4de48f0f2756f4795
absolitudo <absolitudo@gmail.com>
2017-05-25 17:11:49 +0200

    more file structure

--- dungeon-crawler/src/components/canvas.js
+++ dungeon-crawler/src/components/canvas.js
@@ -1,12 +1,27 @@
 import React from "react"
-
 import { connect } from "react-redux"
 
+
+
 class CanvasComponent extends React.Component {
+    componentDidMount() {
+        var canvas = this.refs.canvas
+        var c = canvas.getContext("2d")
+
+        canvas.height = 320
+        canvas.width = 320
+
+        var dirt = new Image();
+        dirt.src = "./img/dirt.png";
+        c.drawImage(dirt, 16, 16)
+
+
+    }
     render() {
-        console.log(this.props.character)
         return(
-            <canvas></canvas>
+            <div>
+                <canvas ref="canvas"></canvas>
+            </div>
         )
     }
 }
--- dungeon-crawler/src/index.js
+++ dungeon-crawler/src/index.js
@@ -16,6 +16,7 @@ const store = createStore(reducer);
 
 class App extends React.Component {
     render() {
+
         return(
             <CanvasComponent />
         )
